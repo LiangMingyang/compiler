06c60ca0a2d18cc293a32afbe63aa8b5e20f1892
明阳 <lmysoar@hotmail.com>
2015-01-08 04:16:34 +0800

    神奇的翻译成js，自己玩的非常开心，差几个语句的翻译没有完成：print,scanf,return

---
+++ public/javascripts/build.js
@@ -0,0 +1,5 @@
+/**
+ * Created by 明阳 on 2015/1/8.
+ */
+var A=0;var B=0;var C=0;var a=1;var b=0;var c=-1;function add(a,b){}function main(){var a=0;(((((a))=(((((((b)+(1))))))+(add())))));}main();
+
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /**
  * Created by 明阳 on 2015/1/6.
  */
-var l = require('./lexer.js');
-var p = require('./parser.js');
+var l = require('./public/javascripts/lexer.js');
+var p = require('./public/javascripts/parser.js');
 lexer.setInput('    const a= 1,b=0;');
 var parser = new Parser();
 lexer.lex();
